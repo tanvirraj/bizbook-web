2fca6356bad63c1d9e7c4cf4439101fb7a3ea8a7
tanvirraj <rajbp02@gmail.com>
2020-06-22 22:07:24 +0600

    feat: update content policy and clean sidebar

--- src/ui/SiderMenu/SiderMenu.tsx
+++ src/ui/SiderMenu/SiderMenu.tsx
@@ -63,28 +63,10 @@ const SiderMenu: FunctionComponent<Props> = ({ siderMenu }) => {
     <Menu
       //onClick={this.handleClick}
       theme="dark"
-      style={{ width: 256 }}
-      defaultSelectedKeys={["1"]}
-      defaultOpenKeys={["sub1"]}
+      //defaultSelectedKeys={["1"]}
+      //defaultOpenKeys={["sub1"]}
       mode="inline"
     >
-      <SubMenu
-        key="sub1"
-        title={
-          <span>
-            <span>Navigation One</span>
-          </span>
-        }
-      >
-        <Menu.ItemGroup key="g1" title="Item 1">
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-        </Menu.ItemGroup>
-        <Menu.ItemGroup key="g2" title="Item 2">
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
       <SubMenu key="sub2" title="Navigation Two">
         <Menu.Item key="5">Option 5</Menu.Item>
         <Menu.Item key="6">Option 6</Menu.Item>
@@ -92,19 +74,6 @@ const SiderMenu: FunctionComponent<Props> = ({ siderMenu }) => {
           <Menu.Item key="7">Option 7</Menu.Item>
           <Menu.Item key="8">Option 8</Menu.Item>
         </SubMenu>
-      </SubMenu>
-      <SubMenu
-        key="sub4"
-        title={
-          <span>
-            <span>Navigation Three</span>
-          </span>
-        }
-      >
-        <Menu.Item key="9">Option 9</Menu.Item>
-        <Menu.Item key="10">Option 10</Menu.Item>
-        <Menu.Item key="11">Option 11</Menu.Item>
-        <Menu.Item key="12">Option 12</Menu.Item>
       </SubMenu>
     </Menu>
   );
